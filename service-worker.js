a1cea21c30681745889ad7878ffea8d6c61f7c2b
artfestkps <artfestkps@gmail.com>
2026-09-06 06:52:52 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'waltakun-26-v15';
+const CACHE = 'waltakun-26-v16';
 const SHELL = ['./', './index.html', './wall.html', './styles.css', './app.js', './manifest.webmanifest', './assets/waltakun-logo-red.png'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
